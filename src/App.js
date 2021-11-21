b341e79876081af990af73d7d3712211840aaa36
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-21 08:54:40 +0100

    add some data in home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
+    <div> 
     <div className='container bg-blue-500 text-center p-5'>
       App and conatiners
+    </div>
+    <div>
+      <Home/>
+    </div>
     </div>
   );
 }
