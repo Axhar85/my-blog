578d5bfe1d43f3bfa5cfb40442b25fdc4a69181f
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-22 10:58:06 +0100

    router working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Home from './pages/Home';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Routes } from 'react-router';
 
 function App() {
   return (
-   
+    <Router> 
     <div className='max-w-screen-md mx-auto pt-20'>
+      <Routes> 
+      <Route exact path='/' element={<Home />} /> 
       
-      <Home/>
-    
+      </Routes>
     </div>
+    </Router>
   );
 }
 
