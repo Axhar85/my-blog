294e369faef175f85d43d48a2e2c191712312cb1
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-23 12:24:17 +0100

    linked articles

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,15 +1,21 @@
 import React from "react";
+import artilceContent from "./article-content"
 
 const Article = ({ match }) => {
     const name = match.params.name;
+    const article = artilceContent.find((article) => article.name === name);
     return (
         <>
          <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
-             This is {name} Article
+             {article.title}
         </h1>
-
+            {article.content.map((paragraph, index) => (
+        <p className='mx-auto leading-relaxed text-base mb-4' key={index}>
+          {paragraph}
+        </p>
+            ))}
         </>
-    )
+    );
 };
 
 export default Article;
