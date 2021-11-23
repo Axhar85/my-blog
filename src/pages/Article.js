f414ec02d1c52fb29c6bf200fafb28431b5dcb62
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-23 12:26:25 +0100

    article not exist added

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -4,6 +4,7 @@ import artilceContent from "./article-content"
 const Article = ({ match }) => {
     const name = match.params.name;
     const article = artilceContent.find((article) => article.name === name);
+    if (!article) return <h1>Article doesn't exists.</h1>
     return (
         <>
          <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
