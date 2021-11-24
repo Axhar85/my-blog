2e6020bd2d8e207652a346b99270947a78651600
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-24 14:30:36 +0100

    adding in article-list the artilces

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import articleContent from './article-content'
 
 const ArticleList = () => {
@@ -7,7 +8,18 @@ const ArticleList = () => {
          <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
              ArticleLict
         </h1>
-        
+        <div className="container py-4 mx-auto">
+            <div className="flex flex-wrap -m-4">
+                {articleContent.map((article, index) => (
+                    <div key={index} className="p-4 md:w-1/2"> 
+                        <div className="h-full border-2 border-gray-200 border-opacity-60 round-lg
+                        overflow-hidden">
+                            <Link to={`/article/${article.name}`}> </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
 
         </>
     )
