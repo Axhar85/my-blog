e0771c9a36b296490b560e14c7a3cd14d327d03f
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-26 11:15:56 +0100

    passing propr other articles to aritlce

--- src/pages/ArticleList.js
+++ src/pages/ArticleList.js
@@ -6,7 +6,7 @@ const ArticleList = () => {
     return (
         <>
          <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
-             Articlea
+             Articleas
         </h1>
         <div className="container py-4 mx-auto">
             <div className="flex flex-wrap -m-4">
